96c51bdcd479824fc64cded31b377b2a3042dbab
Bogdan Bogush <bogdan.bogush@globallogic.com>
2023-11-19 14:59:35 +0200

    Change DM to 104 v2

--- dm_handler.c
+++ dm_handler.c
@@ -24,21 +24,29 @@
 #include "conf_mng.h"
 #include "process_mng.h"
 #include "cmd_intf.h"
+#include "log.h"
 
 #define DM_OBJ_VOICE_SERVICE "Device.Services.VoiceService."
 #define DM_OBJ_VOICE_SERVICE_1 DM_OBJ_VOICE_SERVICE "1."
-#define DM_OBJ_VOICE_PROFILE DM_OBJ_VOICE_SERVICE_1 "VoiceProfile."
-#define DM_OBJ_VOICE_PROFILE_1 DM_OBJ_VOICE_PROFILE "1."
-#define DM_PARAM_SIP_PROXY_SERVER DM_OBJ_VOICE_PROFILE_1 \
-    "SIP.ProxyServer"
-#define DM_PARAM_SIP_PROXY_SERVER_PORT DM_OBJ_VOICE_PROFILE_1 \
-    "SIP.ProxyServerPort"
-#define DM_PARAM_LINE_SIP_AUTH_USER_NAME DM_OBJ_VOICE_PROFILE_1 \
-    "Line.1.SIP.AuthUserName"
-#define DM_PARAM_LINE_SIP_AUTH_PASSWORD DM_OBJ_VOICE_PROFILE_1 \
-    "Line.1.SIP.AuthPassword"
-#define DM_PARAM_LINE_STATUS DM_OBJ_VOICE_PROFILE_1 "Line.1.Status"
-#define DM_PARAM_LINE_ENABLE DM_OBJ_VOICE_PROFILE_1 "Line.1.Enable"
+#define DM_OBJ_VOICE_SIP DM_OBJ_VOICE_SERVICE_1 "SIP."
+#define DM_OBJ_VOICE_SIP_CLIENT DM_OBJ_VOICE_SIP "Client."
+#define DM_OBJ_VOICE_SIP_CLIENT_1 DM_OBJ_VOICE_SIP_CLIENT "1."
+#define DM_PARAM_SIP_CLIENT_AUTH_USER_NAME DM_OBJ_VOICE_SIP_CLIENT_1 \
+    "AuthUserName"
+#define DM_PARAM_SIP_CLIENT_AUTH_PASSWORD DM_OBJ_VOICE_SIP_CLIENT_1 \
+    "AuthPassword"
+#define DM_PARAM_SIP_CLIENT_ENABLE DM_OBJ_VOICE_SIP_CLIENT_1 \
+    "Enable"
+#define DM_PARAM_SIP_CLIENT_STATUS DM_OBJ_VOICE_SIP_CLIENT_1 \
+    "Status"
+
+#define DM_OBJ_VOICE_SIP_NETWORK DM_OBJ_VOICE_SIP "Network."
+#define DM_OBJ_VOICE_SIP_NETWORK_1 DM_OBJ_VOICE_SIP_NETWORK "1."
+
+#define DM_PARAM_NETWORK_OUTBOUND_PROXY DM_OBJ_VOICE_SIP_NETWORK_1 \
+    "OutboundProxy"
+#define DM_PARAM_NETWORK_OUTBOUND_PROXY_PORT DM_OBJ_VOICE_SIP_NETWORK_1 \
+    "OutboundProxyPort"
 
 typedef int (*set_func_t)(char *value);
 typedef int (*get_func_t)(char *req_name, dm_resp_cb_t resp_cb, void *context);
@@ -61,7 +69,7 @@ static int dm_param_proxy_server_get(char *req_name, dm_resp_cb_t resp_cb,
     int ret;
     char *proxy_server = conf_sip_proxy_server_get();
 
-    ret = resp_cb(DM_PARAM_SIP_PROXY_SERVER, proxy_server ? proxy_server : "",
+    ret = resp_cb(DM_PARAM_NETWORK_OUTBOUND_PROXY, proxy_server ? proxy_server : "",
         DM_PARAM_STRING, context);
 
     free(proxy_server);
@@ -82,7 +90,7 @@ static int dm_param_proxy_server_port_get(char *req_name, dm_resp_cb_t resp_cb,
 
     snprintf(str_port_buf, sizeof(str_port_buf), "%u", proxy_server_port);
 
-    return resp_cb(DM_PARAM_SIP_PROXY_SERVER_PORT, str_port_buf,
+    return resp_cb(DM_PARAM_NETWORK_OUTBOUND_PROXY_PORT, str_port_buf,
         DM_PARAM_UNSIGNED_INTEGER, context);
 }
 
@@ -97,7 +105,7 @@ static int dm_param_sip_username_get(char *req_name, dm_resp_cb_t resp_cb,
     int ret;
     char *username = conf_sip_username_get();
 
-    ret = resp_cb(DM_PARAM_LINE_SIP_AUTH_USER_NAME, username ? username : "",
+    ret = resp_cb(DM_PARAM_SIP_CLIENT_AUTH_USER_NAME, username ? username : "",
         DM_PARAM_STRING, context);
 
     free(username);
@@ -116,7 +124,7 @@ static int dm_param_sip_password_get(char *req_name, dm_resp_cb_t resp_cb,
     int ret;
     char *password = conf_sip_password_get();
 
-    ret = resp_cb(DM_PARAM_LINE_SIP_AUTH_PASSWORD, password ? password : "",
+    ret = resp_cb(DM_PARAM_SIP_CLIENT_AUTH_PASSWORD, password ? password : "",
         DM_PARAM_STRING, context);
 
     free(password);
@@ -135,14 +143,14 @@ static int dm_param_status_get(char *req_name, dm_resp_cb_t resp_cb,
 
     if (!enabled)
     {
-        ret = resp_cb(DM_PARAM_LINE_STATUS, "Disabled", DM_PARAM_STRING,
+        ret = resp_cb(DM_PARAM_SIP_CLIENT_STATUS, "Disabled", DM_PARAM_STRING,
             context);
         goto Exit;
     }
 
     if (username == NULL || proxy_server == NULL)
     {
-        ret = resp_cb(DM_PARAM_LINE_STATUS, "Error", DM_PARAM_STRING,
+        ret = resp_cb(DM_PARAM_SIP_CLIENT_STATUS, "Error", DM_PARAM_STRING,
             context);
         goto Exit;
     }
@@ -156,19 +164,19 @@ static int dm_param_status_get(char *req_name, dm_resp_cb_t resp_cb,
     switch (reg_status)
     {
     case SIP_REG_UNREGISTERED:
-        ret = resp_cb(DM_PARAM_LINE_STATUS, "Registering", DM_PARAM_STRING,
+        ret = resp_cb(DM_PARAM_SIP_CLIENT_STATUS, "Registering", DM_PARAM_STRING,
             context);
         break; 
     case SIP_REG_REGISTERED:
-        ret = resp_cb(DM_PARAM_LINE_STATUS, "Up", DM_PARAM_STRING, context);
+        ret = resp_cb(DM_PARAM_SIP_CLIENT_STATUS, "Up", DM_PARAM_STRING, context);
         break;    
     case SIP_REG_REJECTED:
-        ret = resp_cb(DM_PARAM_LINE_STATUS, "Error", DM_PARAM_STRING, context);
+        ret = resp_cb(DM_PARAM_SIP_CLIENT_STATUS, "Error", DM_PARAM_STRING, context);
         break;
     case SIP_REG_NOT_FOUND:
-        ret = resp_cb(DM_PARAM_LINE_STATUS, "Initializing", DM_PARAM_STRING,
+        ret = resp_cb(DM_PARAM_SIP_CLIENT_STATUS, "Initializing", DM_PARAM_STRING,
             context);
-        break;   
+        break;
     default:
         ret = -1;
         break;
@@ -187,7 +195,7 @@ static int dm_param_enable_get(char *req_name, dm_resp_cb_t resp_cb,
     int ret;
     int enabled = conf_sip_enabled_get();
 
-    ret = resp_cb(DM_PARAM_LINE_ENABLE, enabled ? "Enabled" : "Disabled",
+    ret = resp_cb(DM_PARAM_SIP_CLIENT_ENABLE, enabled ? "1" : "0",
         DM_PARAM_STRING, context);
 
     return ret;
@@ -195,21 +203,21 @@ static int dm_param_enable_get(char *req_name, dm_resp_cb_t resp_cb,
 
 static int dm_param_enable_set(char *value)
 {
-    return conf_sip_enabled_set(!strcmp(value, "Enabled"));
+    return conf_sip_enabled_set(!strcmp(value, "1"));
 }
 
 static dm_param_t params[] =
 {
-    { DM_PARAM_SIP_PROXY_SERVER, dm_param_proxy_server_set,
+    { DM_PARAM_NETWORK_OUTBOUND_PROXY, dm_param_proxy_server_set,
         dm_param_proxy_server_get },
-    { DM_PARAM_SIP_PROXY_SERVER_PORT, dm_param_proxy_server_port_set, 
+    { DM_PARAM_NETWORK_OUTBOUND_PROXY_PORT, dm_param_proxy_server_port_set, 
         dm_param_proxy_server_port_get },
-    { DM_PARAM_LINE_SIP_AUTH_USER_NAME, dm_param_sip_username_set,
+    { DM_PARAM_SIP_CLIENT_AUTH_USER_NAME, dm_param_sip_username_set,
         dm_param_sip_username_get },
-    { DM_PARAM_LINE_SIP_AUTH_PASSWORD, dm_param_sip_password_set,
+    { DM_PARAM_SIP_CLIENT_AUTH_PASSWORD, dm_param_sip_password_set,
         dm_param_sip_password_get },
-    { DM_PARAM_LINE_STATUS, NULL, dm_param_status_get },
-    { DM_PARAM_LINE_ENABLE, dm_param_enable_set, dm_param_enable_get },
+    { DM_PARAM_SIP_CLIENT_STATUS, NULL, dm_param_status_get },
+    { DM_PARAM_SIP_CLIENT_ENABLE, dm_param_enable_set, dm_param_enable_get },
 };
 
 int dm_param_set(char *name, char *value, dm_param_type_t type)
@@ -246,9 +254,7 @@ int dm_param_get(char *req_name, dm_resp_cb_t resp_cb, void *context)
 
     /* Handle object request */
     if (!strcmp(req_name, DM_OBJ_VOICE_SERVICE) ||
-        !strcmp(req_name, DM_OBJ_VOICE_SERVICE_1) ||
-        !strcmp(req_name, DM_OBJ_VOICE_PROFILE) ||
-        !strcmp(req_name, DM_OBJ_VOICE_PROFILE_1))
+        !strcmp(req_name, DM_OBJ_VOICE_SERVICE_1))
     {
         for (i = 0; i < sizeof(params) / sizeof(params[0]); i++)
         {
